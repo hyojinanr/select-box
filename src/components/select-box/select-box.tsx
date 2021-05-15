a4209899073d680e05d2168aaee0db820f148005
Rooie <rooie@Rooieui-MacBookAir.local>
2021-05-16 04:20:30 +0900

    add select-box style

--- src/components/select-box/select-box.tsx
+++ src/components/select-box/select-box.tsx
@@ -2,23 +2,32 @@ import React, { Component } from 'react';
 
 export default class SelectBox extends Component {
    state = {
-      selectValues: []
+      selectedValues: []
    };
    
    constructor(props:any) {
       super(props);
-      this.state.selectValues = [];
+      this.state.selectedValues = [];
    }
 
    render() {
       return (
-      <select>
-         <option selected value="">선택</option>
-         <option value="1">1</option>
-         <option value="2">2</option>
-         <option value="3">3</option>
-         <option value="4">4</option>
-      </select>
+         <div className="select-box">
+            <select>
+               <option selected value="">선택</option>
+               <option value="1">1</option>
+               <option value="2">2</option>
+               <option value="3">3</option>
+               <option value="4">4</option>
+            </select>
+            <div className="selected-value">선택</div>
+            <div className="options">
+               <div className="option selected">1</div>
+               <div className="option">2</div>
+               <div className="option">3</div>
+               <div className="option">4</div>
+            </div>
+      </div>
       );
    }
 }
