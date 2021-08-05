9be6ae03546a79552a09a215da25c1f87b3dc697
hyojinanr <hyojinanr@gmail.com>
2021-08-05 23:02:59 +0900

    옵션명 변경

--- src/App.tsx
+++ src/App.tsx
@@ -16,10 +16,10 @@ class App extends Component {
 
   render() {
     const testData : OptionSet[] = [
-      { value: '더 기프팅 컴퍼니', label: '더 기프팅 컴퍼니'},
-      { value: '월간 가슴', label: '월간 가슴' },
-      { value: '인더웨어', label: '인더웨어' },
-      { value: '프론트엔드 엔지니어', label: '프론트엔드 엔지니어' }
+      { value: 'Option 1', label: 'Option 1'},
+      { value: 'Option 2', label: 'Option 2' },
+      { value: 'Option 3', label: 'Option 3' },
+      { value: 'Option 4', label: 'Option 4' }
     ];
   
     return (
